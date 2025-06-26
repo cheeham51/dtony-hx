fa8f31c5523e734f99f1b2a2b9334213904aa39a
Tony Du <cheeham51@hotmail.com>
2025-06-26 12:08:59 +1000

    render task def

--- app/app.py
+++ app/app.py
@@ -2,6 +2,11 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
         if self.path == "/hello":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
